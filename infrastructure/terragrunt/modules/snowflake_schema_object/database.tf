0cc5e65c5fb01556f99674452f2eea7098c60bb4
okayamatakuma <2takumax@gmail.com>
2025-07-20 11:56:57 +0900

    いらないファイル削除

--- infrastructure/terragrunt/modules/snowflake_schema_object/database.tf
+++ infrastructure/terragrunt/modules/snowflake_schema_object/database.tf
@@ -1,8 +1,3 @@
 resource "snowflake_database" "ufc-fight-prediction" {
   name = var.database_name
 }
-
-# resource "snowflake_schema" "RAW" {
-#   name     = "PUBLIC"
-#   database = snowflake_database.ufc-fight-prediction.name
-# }
